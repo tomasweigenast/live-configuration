02ad490274e5d32af07d4c524776aa0b372acd30
Tomas Wegenast <tomaswegenast@gmail.com>
2021-01-19 10:40:59 -0300

    Add Dependency injection extensions

---
+++ packages/dotnet/src/LiveConfiguration.Core/DependencyInjection/DependencyInjectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using LiveConfiguration.Core.Source;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace LiveConfiguration.Core.DependencyInjection
+{
+    /// <summary>
+    /// Provides methods to inject an instance of <see cref="ILiveConfiguration"/> into the services
+    /// </summary>
+    public static class DependencyInjectionExtensions
+    {
+        /// <summary>
+        /// Injects a singleton instance of <see cref="ILiveConfiguration"/>.
+        /// </summary>
+        /// <param name="services">The services to inject the instance to.</param>
+        /// <param name="source">The configuration source.</param>
+        /// <param name="options">The options to configure the instance.</param>
+        public static IServiceCollection AddLiveConfiguration(this IServiceCollection services, ILiveConfigurationSource source, LiveConfigurationOptions options)
+            => services.AddSingleton<ILiveConfiguration>(new DefaultImpl.DefaultLiveConfiguration("DependencyInjection", source, options));
+
+        /// <summary>
+        /// Injects a singleton instance of <see cref="ILiveConfiguration"/>.
+        /// </summary>
+        /// <param name="services">The services to inject the instance to.</param>
+        /// <param name="setupAction">The action to setup the instance.</param>
+        public static IServiceCollection AddLiveConfiguration(this IServiceCollection services, Action<LiveConfigurationSetup> setupAction)
+        {
+            LiveConfigurationSetup setup = new LiveConfigurationSetup();
+            setupAction.Invoke(setup);
+
+            if (setup.Source == null) throw new ArgumentNullException("source", "Source cannot be null.");
+
+            return AddLiveConfiguration(services, setup.Source, setup.Options);
+        }
+    }
+}
